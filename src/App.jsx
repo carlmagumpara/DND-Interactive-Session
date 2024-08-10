adce6b78f6fdb1ad9a31b7af49a48f10f18a481c
Carl Anthony Magumpara <carl@lexmeet.com>
2024-08-10 22:42:46 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,192 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
 import { Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      todo: 'Todo 1',
-      status: 'Done'
+      todo: 'Sample 1',
+      status: 'Done',
     },
     {
       id: 2,
-      todo: 'Todo 2',
-      status: 'Undone'
+      todo: 'Sample 2',
+      status: 'Done',
     },
     {
       id: 3,
-      todo: 'Todo 3',
-      status: 'Done'
+      todo: 'Sample 3',
+      status: 'Undone',
     },
     {
       id: 4,
-      todo: 'Todo 4',
-      status: 'Undone'
+      todo: 'Sample 4',
+      status: 'Done',
     },
     {
       id: 5,
-      todo: 'Todo 5',
-      status: 'Done'
-    },
+      todo: 'Sample 5',
+      status: 'Undone',
+    }
   ]);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selected_item, setSelectedItem] = useState(null);
 
-  const status = ['Undone', 'Ongoing', 'Done'];
+  // onDrop -> parent div
+  // onDragOver -> parent div
+
+  // draggable -> child div
+  // onDragStart -> child div
+  // onDragEnd -> child div
+
+  console.log(selected_item);
 
   return (
-    <>
-      <Container className="mt-5">
-        <Row>
-          {status.map(_item => (
-          <Col>
-            <p className="text-center">{_item} Todos ({todos.filter(item => item.status === _item).length})</p>
-            <div
-              className="p-5 border"
-              onDrop={e => {
-                const id = parseInt(e.dataTransfer.getData("Text"));
-                const _todos = [...todos];
-                const selected_todo_index = todos.findIndex(item => item.id === id);
-                _todos[selected_todo_index].status = _item;
-                setTodos(_todos);
-              }}
-              onDragOver={e => {
-                event.stopPropagation();
-                event.preventDefault();
-              }}
-            >
-              <ListGroup className="w-100">
-                {todos.filter(item => item.status === _item).map(item => (
-                  <ListGroup.Item
-                    draggable
-                    onDragStart={e => {
-                      setSelectedItem(item);
-                      e.dataTransfer.setData("Text", item.id);
-                    }}
-                    onDragEnd={() => setSelectedItem(null)}
-                    style={{ opacity: selectedItem?.id === item.id ? 0.5 : 1 }}
-                  >
-                    {item.todo}
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            </div>
-          </Col>
-          ))}
-        </Row>
-      </Container>
-    </>
-  )
+    <Container className="pt-5">
+      <Row>
+        <Col>
+          <p>Undone</p>
+          <ListGroup
+            className="pt-5 pb-5"
+            onDragOver={e => e.preventDefault()}
+            onDrop={e => {
+              const id = parseInt(e.dataTransfer.getData('Text')); // get dataTransfer data
+              const _todos = [...todos]; // copy to make it mutable
+              const index = _todos.findIndex(item => item.id === id); // find index by value
+              _todos[index].status = 'Undone'; // set status
+              setTodos(_todos); // update todos state
+              setSelectedItem(null); // reset selected item
+            }}
+          >
+            {todos.filter(item => item.status === 'Undone').map(item => (
+              <ListGroup.Item
+                key={item.id}
+                draggable
+                onDragStart={e => {
+                  e.dataTransfer.setData('Text', item.id); // hold the value to another element, its like context API
+                  setSelectedItem(item);
+                }}
+                onDragEnd={() => alert('end')}
+                style={{ border: item.id === selected_item?.id ? '1px solid red' : 'none' }}
+              >
+                {item.todo}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col>
+          <p>Done</p>
+          <ListGroup
+            className="pt-5 pb-5"
+            onDragOver={e => e.preventDefault()}
+            onDrop={e => {
+              const id = parseInt(e.dataTransfer.getData('Text')); // get dataTransfer data
+              const _todos = [...todos]; // copy to make it mutable
+              const index = _todos.findIndex(item => item.id === id); // find index by value
+              _todos[index].status = 'Done'; // set status
+              setTodos(_todos); // update todos state
+              setSelectedItem(null); // reset selected item
+            }}
+          >
+            {todos.filter(item => item.status === 'Done').map(item => (
+              <ListGroup.Item
+                key={item.id}
+                draggable
+                onDragStart={e => {
+                  e.dataTransfer.setData('Text', item.id); // hold the value to another element, its like context API
+                  setSelectedItem(item);
+                }}
+                onDragEnd={() => setSelectedItem(null)}
+                style={{ border: item.id === selected_item?.id ? '1px solid red' : 'none' }}
+              >
+                {item.todo}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-
-// function App() {
-//   const [todos, setTodos] = useState([
-//     {
-//       id: 1,
-//       todo: 'Todo 1',
-//       status: 'Done'
-//     },
-//     {
-//       id: 2,
-//       todo: 'Todo 2',
-//       status: 'Undone'
-//     },
-//     {
-//       id: 3,
-//       todo: 'Todo 3',
-//       status: 'Done'
-//     },
-//     {
-//       id: 4,
-//       todo: 'Todo 4',
-//       status: 'Undone'
-//     },
-//     {
-//       id: 5,
-//       todo: 'Todo 5',
-//       status: 'Done'
-//     },
-//   ]);
-//   const [selectedItem, setSelectedItem] = useState(null);
-
-//   return (
-//     <>
-//       <Container className="mt-5">
-//         <Row>
-//           <Col>
-//             <p>Done Todos</p>
-//             <div
-//               className="p-5"
-//               onDrop={e => {
-//                 const id = parseInt(e.dataTransfer.getData("Text"));
-//                 const _todos = [...todos];
-//                 const selected_todo_index = todos.findIndex(item => item.id === id);
-//                 _todos[selected_todo_index].status = 'Done';
-//                 setTodos(_todos);
-//               }}
-//               onDragOver={e => {
-//                 event.stopPropagation();
-//                 event.preventDefault();
-//               }}
-//             >
-//               <ListGroup>
-//                 {todos.filter(item => item.status === 'Done').map(item => (
-//                   <ListGroup.Item
-//                     draggable
-//                     onDragStart={e => {
-//                       setSelectedItem(item);
-//                       e.dataTransfer.setData("Text", item.id);
-//                     }}
-//                     onDragEnd={() => setSelectedItem(null)}
-//                     style={{ opacity: selectedItem?.id === item.id ? 0.5 : 1 }}
-//                   >
-//                     {item.todo}
-//                   </ListGroup.Item>
-//                 ))}
-//               </ListGroup>
-//             </div>
-//           </Col>
-//           <Col>
-//             <p>Undone Todos</p>
-//             <div
-//               className="p-5"
-//               onDrop={e => {
-//                 const id = parseInt(e.dataTransfer.getData("Text"));
-//                 const _todos = [...todos];
-//                 const selected_todo_index = todos.findIndex(item => item.id === id);
-//                 _todos[selected_todo_index].status = 'Undone';
-//                 setTodos(_todos);
-//               }}
-//               onDragOver={e => {
-//                 event.stopPropagation();
-//                 event.preventDefault();
-//               }}
-//             >
-//               <ListGroup>
-//                 {todos.filter(item => item.status === 'Undone').map(item => (
-//                   <ListGroup.Item
-//                     draggable
-//                     onDragStart={e => {
-//                       setSelectedItem(item);
-//                       e.dataTransfer.setData("Text", item.id);
-//                     }}
-//                     onDragEnd={() => setSelectedItem(null)}
-//                     style={{ opacity: selectedItem?.id === item.id ? 0.5 : 1 }}
-//                   >
-//                     {item.todo}
-//                   </ListGroup.Item>
-//                 ))}
-//               </ListGroup>
-//             </div>
-//           </Col>
-//         </Row>
-//       </Container>
-//     </>
-//   )
-// }
-
-export default App
+export default App;
